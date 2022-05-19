c1d352600de90bb5c0ff1325189c59d3f7ec7c06
AttractiveMinki <wnduq125@naver.com>
2022-05-19 06:33:07 +0000

    fix: divide output_unit by subword, and character

--- dataset/kspon/preprocess.sh
+++ dataset/kspon/preprocess.sh
@@ -7,14 +7,26 @@ OUTPUT_UNIT='character'                                          # you can set c
 PREPROCESS_MODE='phonetic'                                       # phonetic : 칠 십 퍼센트,  spelling : 70%
 VOCAB_SIZE=10000                                                  # if you use subword output unit, set vocab size
 
-echo "Pre-process KsponSpeech Dataset.."
+# echo "Pre-process KsponSpeech Dataset.."
 
+#### subword ###
+# python main.py \
+# --dataset_path $DATASET_PATH \
+# --vocab_dest $VOCAB_DEST \
+# --output_unit $OUTPUT_UNIT \
+# --preprocess_mode $PREPROCESS_MODE \
+# --vocab_size $VOCAB_SIZE \
+# --output_unit 'subword' \
+# --preprocess_mode "spelling" \
+# --savepath "../../vocab"
+
+### character ###
 python main.py \
 --dataset_path $DATASET_PATH \
 --vocab_dest $VOCAB_DEST \
 --output_unit $OUTPUT_UNIT \
 --preprocess_mode $PREPROCESS_MODE \
 --vocab_size $VOCAB_SIZE \
---output_unit 'subword' \
+--output_unit 'character' \
 --preprocess_mode "spelling" \
 --savepath "../../vocab"
