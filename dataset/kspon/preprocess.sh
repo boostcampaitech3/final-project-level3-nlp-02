eec7d59b7b14d965a67af36a34a2161eabb780f9
AttractiveMinki <wnduq125@naver.com>
2022-05-20 05:27:23 +0000

    fix: change preprocess.sh

--- dataset/kspon/preprocess.sh
+++ dataset/kspon/preprocess.sh
@@ -5,7 +5,8 @@ DATASET_PATH="../../../kspon_dataset/train" #"SET_YOUR_DATASET_PATH"
 VOCAB_DEST="../../vocab"
 OUTPUT_UNIT='character'                                          # you can set character / subword / grapheme
 PREPROCESS_MODE='phonetic'                                       # phonetic : 칠 십 퍼센트,  spelling : 70%
-VOCAB_SIZE=10000                                                  # if you use subword output unit, set vocab size
+VOCAB_SIZE=5000                                                  # if you use subword output unit, set vocab size
+# VOCAB_SIZE=10000                                                  # if you use subword output unit, set vocab size
 
 # echo "Pre-process KsponSpeech Dataset.."
 
@@ -24,9 +25,7 @@ VOCAB_SIZE=10000                                                  # if you use s
 python main.py \
 --dataset_path $DATASET_PATH \
 --vocab_dest $VOCAB_DEST \
---output_unit $OUTPUT_UNIT \
---preprocess_mode $PREPROCESS_MODE \
 --vocab_size $VOCAB_SIZE \
 --output_unit 'character' \
---preprocess_mode "spelling" \
---savepath "../../vocab"
+--preprocess_mode "spelling"
+# --savepath "../../vocab"
