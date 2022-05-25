0a6db6adb4933f17c3caab7257c1ef1bfec6aa22
s1c5000 <s1c5000@naver.com>
2022-05-25 07:53:11 +0000

    fix summary

--- infer.py
+++ infer.py
@@ -2,6 +2,7 @@ import torch
 import streamlit as st
 from kobart import get_kobart_tokenizer
 from transformers.models.bart import BartForConditionalGeneration
+import re
 
 #@st.cache
 @st.cache(hash_funcs={torch.nn.parameter.Parameter: lambda _: None})
@@ -14,6 +15,7 @@ model = load_model()
 tokenizer = get_kobart_tokenizer()
 st.title("KoBART 요약 Test")
 text = st.text_area("뉴스 입력:")
+text = re.sub('\n','.\n', text)
 
 st.markdown("## 뉴스 원문")
 st.write(f'len_text : {len(text)}')
@@ -27,6 +29,19 @@ if text:
         input_ids = torch.tensor(input_ids)
         input_ids = input_ids.unsqueeze(0) # 이 길이가 1024 까지만 들어간다.
         st.write(f'input_shape : {input_ids.shape}')
-        output = model.generate(input_ids, eos_token_id=1, max_length=512, num_beams=5)
-        output = tokenizer.decode(output[0], skip_special_tokens=True)
-    st.write(output)
+        input_ids_list = input_ids.split(1000, dim=-1)
+
+        outputs = ""
+        for inputs in input_ids_list:
+            st.write(inputs.shape)
+            output = model.generate(inputs, eos_token_id=1, max_length=512, num_beams=5)
+            output = tokenizer.decode(output[0], skip_special_tokens=True)
+            outputs += output
+            st.write(output)
+
+    outputs = tokenizer.encode(outputs)
+    outputs = torch.tensor(outputs)
+    outputs = outputs.unsqueeze(0)
+    output_ = model.generate(outputs, eos_token_id=1, max_length=512, num_beams=5)
+    output_ = tokenizer.decode(output_[0], skip_special_tokens=True)
+    st.write(output_)
