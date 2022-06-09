77d11530ad9e84ace742158757efd7a66000e6e1
AttractiveMinki <wnduq125@naver.com>
2022-06-09 09:38:39 +0000

    feat: add keyword

--- bin/backend/main.py
+++ bin/backend/main.py
@@ -11,13 +11,14 @@ import sys
 # import yaml
 from pytube import YouTube
 from moviepy.editor import *
+from transformers import ElectraForPreTraining, ElectraTokenizer, pipeline
 from transformers.models.bart import BartForConditionalGeneration
 from kobart import get_kobart_tokenizer
 import time
 
 # 상위 디렉토리에서 가져오기
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-from utils import dell_loop, get_split, make_specific_link
+from utils import dell_loop, get_split, make_specific_link, summary_post_processing, get_tfidf_vec, get_similar
 from key_bert import load_embeddings, get_candidates, dist_keywords, max_sum_sim, mmr
 
 app = FastAPI()
@@ -35,6 +36,22 @@ def load_model():
     # model_ = BartForConditionalGeneration.from_pretrained('../kobart_summary2_v_1') # minjun 합친거로 학습
     model_ = BartForConditionalGeneration.from_pretrained('../kobart_summary4') # younhye
     return model_
+
+def load_postprocess_model():
+    #koelectra 모델 - 이상한 토큰 찾아서 masking
+    discriminator = ElectraForPreTraining.from_pretrained("monologg/koelectra-base-v3-discriminator")
+    electra_tokenizer = ElectraTokenizer.from_pretrained("monologg/koelectra-base-v3-discriminator")
+
+    # KoBigBird
+    fill_model = pipeline(
+        "fill-mask",
+        model='monologg/kobigbird-bert-base',
+        tokenizer=('monologg/kobigbird-bert-base', {'use_fast':True}),
+        framework = 'pt',
+        top_k  = 1
+        )
+
+    return discriminator, electra_tokenizer, fill_model
 
 
 def get_keyword(text: str, top_n: int=10):
@@ -57,6 +74,11 @@ class Url_check(BaseModel):
 
 
 class list_check(BaseModel):
+    talk_list: list
+
+
+class dict_check(BaseModel):
+    question: list
     talk_list: list
 
 
@@ -205,18 +227,14 @@ def set_voice(specific_url: Url_check):
 # cos 유사도 계산하여 문장 구별
 @app.get("/summary")
 def get_summary(talk_list: list_check):
+    talk_list = talk_list.talk_list
     model_summary = load_model()
+    discriminator, electra_tokenizer, fill_model = load_postprocess_model()
     _ = model_summary.eval()
     tokenizer = get_kobart_tokenizer()
-    # print(KoBART 요약 Test)
-    # print('!!!', talk_list)
-    temp_talk_list = [talk[1] for talk in talk_list]
-    text = '\n'.join(map(str, temp_talk_list))
-    # print('@@@',text)
+    print('!!!', talk_list)
 
-    # print(text)
-    text = text.replace('\n', ' ')
-    split_text_list = get_split(text, tokenizer.tokenize, n=3) # [[문단,0],[문단,21],[문단,46] ... ]
+    split_text_list = get_split(talk_list, tokenizer.tokenize, n=3) # [[문단,0],[문단,21],[문단,46] ... ]
     # print("## KoBART 요약 결과")
     outputs = ""
     for split_text in split_text_list:
@@ -225,12 +243,10 @@ def get_summary(talk_list: list_check):
         input_ids = torch.tensor(input_ids)
         input_ids = input_ids.unsqueeze(0) # 이 길이가 1024개 까지만 들어간다.
 
-        #st.write('본문')
-        #st.write(sp_text)
-        #st.write(f'input_shape : {input_ids.shape}')
         output = model_summary.generate(input_ids, eos_token_id=1, max_length=200, num_beams=5) # eos_token_id=1, max_length=100, num_beams=5)
         output = tokenizer.decode(output[0], skip_special_tokens=True)
         output = dell_loop(output)
+        output = summary_post_processing(generated_summary=output, discriminator=discriminator, tokenizer=electra_tokenizer, fill_model=fill_model)
         outputs += output.replace("'", "").replace(",", "") + "\n"
         #st.write('요약')
         # print(output)
@@ -248,6 +264,7 @@ def get_summary(talk_list: list_check):
 @app.get("/summary_before")
 def get_summary2(talk_list: list_check):
     model_summary = load_model()
+    discriminator, electra_tokenizer, fill_model = load_postprocess_model()
     _ = model_summary.eval()
     tokenizer = get_kobart_tokenizer()
     # print(KoBART 요약 Test)
@@ -265,8 +282,10 @@ def get_summary2(talk_list: list_check):
         output = model_summary.generate(inputs, eos_token_id=1, max_length=300, num_beams=10) # eos_token_id=1, max_length=100, num_beams=5)
         output = tokenizer.decode(output[0], skip_special_tokens=True)
         output = dell_loop(output)
+        ## 후처리
+        output = summary_post_processing(generated_summary=output, discriminator=discriminator, tokenizer=electra_tokenizer, fill_model=fill_model)
         outputs += output
-        # print(output)
+        
     print("outputs1000:### ", outputs)
     # 유효하면 정상 링크 안내
     return JSONResponse(
@@ -292,6 +311,25 @@ def extract_keyword(talk_list: list_check):
                 "message": "완료", 
                 "outputs": outputs
             }
+    )
+
+
+@app.get("/query")
+def question_answer(talk_dict: dict_check):
+    print('###@@@', talk_dict)
+    question = talk_dict.question
+    talk_list = talk_dict.talk_list
+    tokenizer = get_kobart_tokenizer()
+    
+    tfidfv, tfidf_matrix, sec_text_list = get_tfidf_vec(talk_list, tokenizer.tokenize) # 로컬에 tfidf모델이랑 벡터 지정하는게 좋을듯
+    results = get_similar(question, tfidfv, tfidf_matrix, sec_text_list, 5)
+
+    return JSONResponse(
+        status_code=200,
+        content={
+            "message": "완료",
+            "outputs": results
+        }
     )
 
  
